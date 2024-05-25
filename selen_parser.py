dcd9f57035a3a2ef4673c7e299d3bb2ee4447002
HedgehogIvan <124003510+HedgehogIvan@users.noreply.github.com>
2024-05-25 23:13:32 +0300

    Фикс мелких багов и незначительное изменение структуры

--- selen_parser.py
+++ selen_parser.py
@@ -7,8 +7,7 @@ import pandas as pd
 import random
 
 
-def avito_parser(brand, model):
-    proxies = [
+PROXIES = [
         "socks4://37.228.65.107:51032",
         "socks4://85.29.147.90:5678",
         "http://213.230.110.47:3128",
@@ -18,44 +17,54 @@ def avito_parser(brand, model):
         "http://185.74.6.247:8080"
     ]
 
-    def create_link(_brand, _model, _page=None):
-        _brand = _brand.replace(' ', '+')
-        _model = _model.replace(' ', '+')
-        return (
-            f"https://www.avito.ru/moskva/avtomobili?cd=1"
-            f"{f'&p={_page}'if _page else ''}"
-            f"&q="
-            f"{_brand}+"
-            f"{_model}"
-            f"&radius=0&searchRadius=0"
-        )
 
-    def get_driver_with_proxy():
-        if len(proxies) == 0:
-            return None
-        
-        i_proxy = random.randint(0, len(proxies) - 1)
-        proxy = proxies.pop(i_proxy)
+def create_link(_brand, _model, _page=None):
+    _brand = _brand.replace(' ', '+')
+    _model = _model.replace(' ', '+')
+    return (
+        f"https://www.avito.ru/moskva/avtomobili?cd=1"
+        f"{f'&p={_page}' if _page else ''}"
+        f"&q="
+        f"{_brand}+"
+        f"{_model}"
+        f"&radius=0&searchRadius=0"
+    )
 
-        try:
-            agent = UserAgent().chrome
-        except FakeUserAgentError:
-            agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36 Edg/104.0.1293.47"
-        options = uc.ChromeOptions()
-        options.add_argument(f"user-agent={agent}")
-        options.add_argument("--disable-blink-features=AutomationControlled")
-        options.add_argument(f"--proxy-server={proxy}")
 
-        driver = uc.Chrome(options=options)
-        driver.implicitly_wait(20)
-        
-        return driver
+def get_driver_with_proxy(proxies):
+    if len(proxies) == 0:
+        return None
 
+    i_proxy = random.randint(0, len(proxies) - 1)
+    proxy = proxies.pop(i_proxy)
+
+    try:
+        agent = UserAgent().chrome
+    except FakeUserAgentError:
+        agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36 Edg/104.0.1293.47"
+    options = uc.ChromeOptions()
+    options.add_argument(f"user-agent={agent}")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument(f"--proxy-server={proxy}")
+
+    driver = uc.Chrome(options=options)
+    driver.implicitly_wait(20)
+
+    return driver
+
+
+def avito_parser(brand, model):
+    proxies = PROXIES.copy()
     page = None
     attempt_count = 0
-    df_main = pd.DataFrame(columns=["Name", "Link", "Price"])
-    
-    driver = get_driver_with_proxy()
+
+    cars_dict = {
+        "Name": [],
+        "Link": [],
+        "Price": []
+    }
+
+    driver = get_driver_with_proxy(proxies)
 
     while True:
         if driver is None:
@@ -68,7 +77,7 @@ def avito_parser(brand, model):
 
             # Перезапуск драйвера
             driver.quit()
-            driver = get_driver_with_proxy()
+            driver = get_driver_with_proxy(proxies)
 
             continue
 
@@ -89,12 +98,6 @@ def avito_parser(brand, model):
             continue
 
         # Поиск объявлений
-        cars_dict = {
-            "Name": [],
-            "Link": [],
-            "Price": []
-        }
-
         cars = driver.find_elements(by=By.XPATH, value='//div[@data-marker="catalog-serp"]/div[@data-marker="item"]')
 
         if len(cars) > 0:
@@ -110,14 +113,21 @@ def avito_parser(brand, model):
                 cars_dict["Link"].append(link)
                 cars_dict["Price"].append(price)
 
-        # Добавление объявлений
-        df_part = pd.DataFrame(cars_dict)
-        df_main = pd.concat([df_main, df_part], ignore_index=True)
-
         # Поиск стрелки пагинации (next)
-        try:
-            next_page = driver.find_element(by=By.XPATH, value='//a[@data-marker="pagination-button/nextPage"]')
-        except:
+        # Если автомобилей на сайте меньше, чем стандартное кол-во авто на страницу (50),
+        # то к основным объявлениям добавляются объявления из других городов.
+        # Средство пагинации у обоих элементов идентично.
+        # Чтобы избежать ложного срабатывания перехода на следующую страницу,
+        # было решено искать списки с объявлениями.
+        # Если список один, то это нужный переход
+        # Если два, то переход рекламный
+        car_lists = driver.find_elements(by=By.XPATH, value='//div[contains(@class, "items-items")]')
+        if car_lists == 1:
+            try:
+                next_page = driver.find_element(by=By.XPATH, value='//a[@data-marker="pagination-button/nextPage"]')
+            except:
+                break
+        else:
             break
 
         # Если страница не указана (None), но есть кнопка перехода, то следующая страница будет 2-ой
@@ -129,4 +139,6 @@ def avito_parser(brand, model):
         nap = random.uniform(30, 90)
         sleep(nap)
 
-    return df_main
+    df = pd.DataFrame(cars_dict)
+
+    return df
